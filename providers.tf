9242829a0d24fde87238d2b439d4fa247b7d9672
James Matthews <james.matthews1@justice.gov.uk>
2026-03-30 16:25:57 +0100

    refactor: repo structure

---
+++ providers.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  backend "azurerm" {
+  }
+
+  required_providers {
+    azurerm = {
+      source                = "hashicorp/azurerm"
+      configuration_aliases = [azurerm.hub, azurerm.log_analytics]
+      version               = ">= 4.65.0"
+    }
+
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.9"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  resource_provider_registrations = "none"
+}
+
+provider "azurerm" {
+  alias = "hub"
+  features {}
+  resource_provider_registrations = "none"
+  subscription_id                 = var.hub_subscription_id
+}
+
+provider "azurerm" {
+  alias = "log_analytics"
+  features {}
+  resource_provider_registrations = "none"
+  subscription_id                 = local.log_analytics_subscription_id
+}
+
+provider "azapi" {
+}
